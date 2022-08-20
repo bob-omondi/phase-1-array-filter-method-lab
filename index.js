2554b3faaa18773c29ccfdd6f9e9bc2446e08525
+ <+>
2022-08-20 16:10:22 +0300

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, characteristic) {
+    return drivers.filter(function (driver) { return driver.toLowerCase() === characteristic.toLowerCase() })
+  }
+  
+  function fuzzyMatch(drivers, characteristic) {
+    return drivers.filter(function (driver) { return driver.toLowerCase().substring(0, characteristic.length) === characteristic.toLowerCase() })
+  }
+  
+  function matchName(drivers, argument) {
+    return drivers.filter(function (driver) { return driver.name === argument })
+  }
